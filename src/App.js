ecc68297f6be55e655d740b9f9910e15dce22fbf
danielrpg <daniel.fernandezgp@gmail.com>
2019-05-20 22:31:00 -0400

    Add bootstrap to our application

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          CRUD React
       </header>
+      <div clasName="container">
+        <h2 className="text-center p-4">Todo App</h2>
+          <ul className="list-group">
+            <li className="list-group-item">SOEM LIST ITEM</li>
+            <li className="list-group-item">WRITE SOME CODE</li>
+            <li className="list-group-item">WATCH BAHDCAST</li>
+          </ul>
+      </div>   
     </div>
   );
 }
