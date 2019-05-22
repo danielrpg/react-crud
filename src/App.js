a0d35d296f093dc59a58e8745258fabf4af8b269
danielrpg <daniel.fernandezgp@gmail.com>
2019-05-21 22:09:55 -0400

    Map function Added

--- src/App.js
+++ src/App.js
@@ -2,30 +2,43 @@ import React,{ Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  }
+];
+
 class App extends Component {
 
-  // constructor() {
-
-  //   super();
-
-  //   this.state = {
-      
-  //     todos :[{
-        
-  //       id: 2, 
-  //       name: 'Buy some clothes' 
-      
-  //     }]
-
-  //   };
-
-  // }
-
   render() {
-    const helloWorld = 'Welcome to the React JS world';
+    
     return (
       <div className="App">
-        <h2> {helloWorld} </h2>
+        {list.map(item => {
+          return (
+            <div key={item.objectId}>
+              <span>
+                <a href={item.url}> {item.title} </a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );  
+        })}
       </div>
     );
        
