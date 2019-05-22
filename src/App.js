92d732e2aaf079d6d9c4251c99736ecd7c57610b
danielrpg <daniel.fernandezgp@gmail.com>
2019-05-21 21:47:19 -0400

    Add hot module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          CRUD React
-      </header>
-      <div clasName="container">
-        <h2 className="text-center p-4">Todo App</h2>
-          <ul className="list-group">
-            <li className="list-group-item">SOEM LIST ITEM</li>
-            <li className="list-group-item">WRITE SOME CODE</li>
-            <li className="list-group-item">WATCH BAHDCAST</li>
-          </ul>
-      </div>   
-    </div>
-  );
+class App extends Component {
+
+  // constructor() {
+
+  //   super();
+
+  //   this.state = {
+      
+  //     todos :[{
+        
+  //       id: 2, 
+  //       name: 'Buy some clothes' 
+      
+  //     }]
+
+  //   };
+
+  // }
+
+  render() {
+    const helloWorld = 'Welcome to the React JS world';
+    return (
+      <div className="App">
+        <h2> {helloWorld} </h2>
+      </div>
+    );
+       
+  }
+
 }
 
 export default App;
